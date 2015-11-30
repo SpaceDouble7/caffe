1312606745b58bfc4abb040d35e368f07c765b8c
blockhead <1026737314@qq.com>
2015-11-30 15:40:20 +0800

    添加回归的参数的定义

---
+++ include/caffe/zy_define.hpp
@@ -0,0 +1,13 @@
+//
+// Created by dell on 2015/11/30.
+//
+
+#ifndef CAFFE_ZY_DEFINE_HPP
+#define CAFFE_ZY_DEFINE_HPP
+
+namespace zy_define {
+    // 定义回归的值的数目，如果需要回归其他的数目的值，则需要更改该变量的值
+    const int REGRESSION_NUM = 3;
+}
+
+#endif //CAFFE_ZY_DEFINE_HPP
